616f0572fbf4bde5975025c6fff1e34fae774a3f
Guest User <Guest@Epicodus-5B.local>
2017-10-27 09:16:42 -0700

    create initial project directory

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>Zoo Animal Tracker</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
